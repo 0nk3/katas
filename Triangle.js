be5690ec75ebe19801a640b51ab03952f688b10a
0nk3 <0nk3.0nk3@gmail.com>
2020-06-16 12:35:34 +0200

    Triangle complete

--- Triangle.js
+++ Triangle.js
@@ -1,10 +1,14 @@
+console.log("Exercise : Draw a right handed triangle\n");
 function triangle(size) {
     var output = "";
-    for (var i = 0; i < size; i++) {
+    for (var i = 0; i <= size; i++) {
         for (var j = 0; j < i; j++) {
             output += "#";
         }
-        console.log(output);
+        output += "\n";
     }
+    console.log(output);
 }
 triangle(2);
+console.log();
+triangle(4);
--- Triangle.ts
+++ Triangle.ts
@@ -1,11 +1,14 @@
 console.log("Exercise : Draw a right handed triangle\n");
 function triangle(size:number) {
     let output:string = "";
-    for (let i:number = 0; i < size; i++){
-        for (let j:number =0 ; j < i; j++){
-            output += "#"
+    for (let i:number = 0; i <= size; i++){
+        for (let j:number = 0 ; j < i; j++){
+            output += "#";
         }
-        console.log(output);
+        output+="\n";
     }
+    console.log(output);
 }
 triangle(2);
+console.log();
+triangle(4);
