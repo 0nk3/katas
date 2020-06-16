55a3f151cc0ab282d15721a3414ed82dff7800bc
0nk3 <0nk3.0nk3@gmail.com>
2020-06-16 14:39:23 +0200

    carter for OOP concepts on the Isosceles Triangle

--- IsoscelesTriangle.js
+++ IsoscelesTriangle.js
@@ -2,22 +2,28 @@
  * @author : Ndumiso Onke Fanti             *
  * Exercise : Draw an Isosceles Triangle    *
  * **************************************** */
-function isosceles(n) {
-    var str = "";
-    //This is the upper part of the isosceles triangle
-    for (var rows = 1; rows <= n; rows++) {
-        for (var j = n + 1; j >= rows; j--) { //dealing with the trailing spaces
-            str += " ";
-        }
-        //dealing with trailing spaces
-        var hashes = "";
-        // printing out the '#'s, numbers of hashes per row is is always odd
-        for (var k = 1; k <= 2 * rows - 1; k++) {
-            hashes += "#";
-        }
-        console.log(str + hashes);
-        str = "";
-        //console.log(" ")
+var IsoscelesTriangle = /** @class */ (function () {
+    function IsoscelesTriangle() {
     }
-}
-isosceles(5);
+    IsoscelesTriangle.prototype.isosceles = function (n) {
+        var str = "";
+        //This is the upper part of the isosceles triangle
+        for (var rows = 1; rows <= n; rows++) {
+            for (var j = n + 1; j >= rows; j--) { //dealing with the trailing spaces
+                str += " ";
+            }
+            //dealing with trailing spaces
+            var hashes = "";
+            // printing out the '#'s, numbers of hashes per row is is always odd
+            for (var k = 1; k <= 2 * rows - 1; k++) {
+                hashes += "#";
+            }
+            console.log(str + hashes);
+            str = "";
+            //console.log(" ")
+        }
+    };
+    return IsoscelesTriangle;
+}());
+var iso = new IsoscelesTriangle();
+iso.isosceles(4);
--- IsoscelesTriangle.ts
+++ IsoscelesTriangle.ts
@@ -2,22 +2,25 @@
  * @author : Ndumiso Onke Fanti             *
  * Exercise : Draw an Isosceles Triangle    *
  * **************************************** */
-function isosceles(n : number) {
-    let str:string = "";
-    //This is the upper part of the isosceles triangle
-    for (let rows:number = 1; rows <= n; rows++){
-        for (let j:number = n + 1; j >= rows; j--){ //dealing with the trailing spaces
-            str+=" ";
-        }
-        //dealing with trailing spaces
-        let hashes:string = "";
-        // printing out the '#'s, numbers of hashes per row is is always odd
-        for (let k:number = 1; k <= 2*rows -1 ; k++){
-            hashes+="#";
-        }
-        console.log(str +hashes)
-        str = "";
-        //console.log(" ")
-    }
+ class IsoscelesTriangle {
+      isosceles(n : number) {
+         let str:string = "";
+         //This is the upper part of the isosceles triangle
+         for (let rows:number = 1; rows <= n; rows++){
+             for (let j:number = n + 1; j >= rows; j--){ //dealing with the trailing spaces
+                 str+=" ";
+             }
+             //dealing with trailing spaces
+             let hashes:string = "";
+             // printing out the '#'s, numbers of hashes per row is is always odd
+             for (let k:number = 1; k <= 2*rows -1 ; k++){
+                 hashes+="#";
+             }
+             console.log(str +hashes)
+             str = "";
+             //console.log(" ")
+         }
+     }
 }
-isosceles(5);
+let iso = new IsoscelesTriangle();
+iso.isosceles(4);
