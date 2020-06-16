28824f76a2440ffa7129b85c110b7b4bd0ec58f0
0nk3 <0nk3.0nk3@gmail.com>
2020-06-16 13:11:26 +0200

    enclose method inside a class to enforce OOP

--- IsoscelesTriangle.js
+++ IsoscelesTriangle.js
@@ -2,14 +2,27 @@
  * @author : Ndumiso Onke Fanti          *
  * Exercise : Find the longest String    *
  *************************************** */
-function longestString(longest) {
-    var temp = "";
-    for (var _i = 0, longest_1 = longest; _i < longest_1.length; _i++) {
-        var word = longest_1[_i];
-        if (temp.length < word.length) {
-            temp = word;
-        }
+var Longest = /** @class */ (function () {
+    function Longest() {
     }
-    return temp.length;
+    Longest.prototype.longestString = function (longest) {
+        var temp = "";
+        for (var _i = 0, longest_1 = longest; _i < longest_1.length; _i++) {
+            var word = longest_1[_i];
+            if (temp.length < word.length) {
+                temp = word;
+            }
+        }
+        return temp.length;
+    };
+    return Longest;
+}());
+var long = new Longest();
+// change the array below to test for a different input
+var arr = ["the", "quick", "brown", "fox", "ate", "my", "chickens", "uuuuuuuu"];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var word = arr_1[_i];
+    if (word.length === long.longestString(arr)) {
+        console.log(word);
+    }
 }
-console.log(longestString(["the", "quick", "brown", "fox", "ate", "my", "chickens"]));
--- IsoscelesTriangle.ts
+++ IsoscelesTriangle.ts
@@ -2,13 +2,24 @@
  * @author : Ndumiso Onke Fanti          *
  * Exercise : Find the longest String    *
  *************************************** */
- function longestString(longest:string[]):number {
-    let temp:string ="";
-    for(let word of longest){
-        if(temp.length < word.length){
-            temp = word;
+class Longest {
+    longestString(longest: string[]): number {
+        let temp: string = "";
+        for (let word of longest) {
+            if (temp.length < word.length) {
+                temp = word;
+            }
         }
+        return temp.length;
     }
-    return temp.length;
- }
- console.log(longestString(["the","quick","brown", "fox", "ate", "my", "chickens"]));
+}
+let long = new Longest();
+
+// change the array below to test for a different input
+let arr:string[] = ["the","quick","brown", "fox", "ate", "my", "chickens"];
+
+for (let word of arr) {
+    if (word.length === long.longestString(arr)){
+        console.log(word);
+    }
+}
